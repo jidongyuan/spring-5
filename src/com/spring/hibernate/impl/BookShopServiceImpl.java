9f9eeb8fb276e58d575fc92cfded0543f1d24545
jidongyuan <1402379857@qq.com>
2017-01-09 23:23:45 +0800

    33课时学习完毕

--- src/com/spring/hibernate/impl/BookShopServiceImpl.java
+++ src/com/spring/hibernate/impl/BookShopServiceImpl.java
@@ -14,6 +14,25 @@ public class BookShopServiceImpl implements BookShopService {
 
     @Autowired
     private BookShopDao bookShopDao;
+
+    /**
+     * spring hibernate事务的流程
+     * 1.在方法之前：
+     * ①.获取session
+     * ②.把session和当前线程绑定，这样就可以在dao中使用sessionFactory的getCurrentSession()方法来获取session
+     * ③.开启事务
+     *
+     * 2.若方法正常结束，即没有出现异常，则
+     * ①.提交事务
+     * ②.使和当前线程绑定的session解除绑定
+     * ③.关闭session
+     *
+     * 3.若方法出现异常，则
+     * ①.回滚事务
+     * ②.使和当前线程绑定的session解除绑定
+     * ③.关闭session
+     *
+     * */
     @Override
     public void purchase(String username, String isbn) {
         int price = bookShopDao.findBookPriceByIsbn(isbn);
--- src/com/spring/hibernate/test/SpringHibernateTest.java
+++ src/com/spring/hibernate/test/SpringHibernateTest.java
@@ -1,12 +1,15 @@
 package com.spring.hibernate.test;
 
 import com.spring.hibernate.service.BookShopService;
+import com.spring.hibernate.service.Cashier;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Administrator on 2016-12-27.
@@ -14,16 +17,24 @@ import java.sql.SQLException;
 public class SpringHibernateTest {
     private ApplicationContext ctx = null;
     private BookShopService bookShopService = null;
+    private Cashier cashier = null;
 
     {
         ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         bookShopService = ctx.getBean(BookShopService.class);
+        cashier = ctx.getBean(Cashier.class);
     }
+
 
     @Test
     public void test() throws SQLException{
         DataSource dataSource = ctx.getBean(DataSource.class);
         System.out.println(dataSource.getConnection());
+    }
+
+    @Test
+    public void testCashier(){
+        cashier.checkout("aa", Arrays.asList("1001","1002"));
     }
 
     @Test
